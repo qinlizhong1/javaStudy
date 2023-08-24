beaecdd24678b0e839872810621e1366cca0317a
lizhong <13590316596@163.com>
2023-08-25 05:51:12 +0800

    logback异步日志 --- 2023/08/25

--- logExample/logback-example-03/src/main/java/LogbackTest.java
+++ logExample/logback-example-03/src/main/java/LogbackTest.java
@@ -1,16 +1,23 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class LogbackExample{
 
     public void test1(){
         System.out.println("----------------------   test1 --------------------");
+        Logger logger = LoggerFactory.getLogger(LogbackExample.class);
+        for (int i = 0; i < 100; i++) {
+            logger.trace("trace级别信息");
+            logger.debug("debug级别信息");
+            logger.info("info级别信息");
+            logger.warn("warn级别信息");
+            logger.error("error级别信息");
+        }
+        for (int i = 0; i < 600; i++) {
+            System.out.println("——————————————");
+        }
     }
 
-    public void test2(){
-        System.out.println("----------------------   test1 --------------------");
-    }
-
-    public void test3(){
-        System.out.println("----------------------   test1 --------------------");
-    }
 }
 
 public class LogbackTest {
