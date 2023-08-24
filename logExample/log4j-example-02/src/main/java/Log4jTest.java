86e6f3d6ff7b7108d81c45f97d0e36e76190a7f7
lizhong <13590316596@163.com>
2023-08-25 01:34:01 +0800

    调整module名称 --- 2023/08/25

--- logExample/log4j-example-02/src/main/java/Log4jTest.java
+++ logExample/log4j-example-02/src/main/java/Log4jTest.java
@@ -1,8 +1,22 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import java.nio.file.attribute.BasicFileAttributes;
 
 class Log4jExample{
     //
     public void test1(){
         System.out.println("----------------------   test1 --------------------");
+        Logger logger = Logger.getLogger(Log4jExample.class);
+
+        logger.trace("trace信息");
+        logger.debug("debug信息");
+        logger.info("info信息");
+        logger.warn("warn信息");
+        logger.error("error信息");
+        logger.fatal("fatal信息");
+
+        BasicConfigurator.configure();
     }
 
     public void test2(){
--- logExample/log4j2-example-01/src/main/java/Log4jTest.java
+++ logExample/log4j2-example-01/src/main/java/Log4jTest.java
@@ -1,21 +1,8 @@
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
 
 class Log4jExample{
-    //不提供配置文件，默认级别为debug
+    //
     public void test1(){
         System.out.println("----------------------   test1 --------------------");
-        //如果没有提供log4j配置文件，要使用 BasicConfigurator.configure() 加载初始化配置
-        BasicConfigurator.configure();
-        //参数为所在
-        Logger logger = Logger.getLogger(Log4jExample.class);
-
-        logger.trace("trace信息");
-        logger.debug("debug信息"); //默认级别
-        logger.info("info信息");
-        logger.warn("warn信息");
-        logger.error("error信息");
-        logger.fatal("fatal信息");
     }
 
     public void test2(){
